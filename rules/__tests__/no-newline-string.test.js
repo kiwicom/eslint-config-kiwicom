19a57c10eb7d9c8b86390edb847ce50f06aa9e8a
Martin Zlámal <mrtnzlml@gmail.com>
2018-12-10 12:08:52 -0500

    Eslint: add new rule 'no-newline-string' to enforce os.EOL usage
    This rule should probably be used only for external NPM packages. Also, it doesn't support template literals yet.
    
    Closes: https://gitlab.skypicker.com/graphql/graphql/issues/384

---
+++ rules/__tests__/no-newline-string.test.js
@@ -0,0 +1,37 @@
+// @flow
+
+import rule from '../no-newline-string';
+
+const RuleTester = require('eslint').RuleTester;
+
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 7,
+  },
+});
+
+const errors = [
+  { message: 'Unexpected newline character (use os.EOL instead).' },
+];
+
+ruleTester.run('no-newline-string', rule, {
+  // please note: characters are double-escaped here
+
+  valid: [
+    { code: 'var x = true' },
+    { code: 'var x = "just a normal string"' },
+    { code: 'var x = "\\\\n"' },
+  ],
+
+  invalid: [
+    {
+      code: 'var x = "\\n"',
+      errors,
+    },
+    {
+      code: 'var x = "just a normal\\nstring"',
+      errors,
+    },
+  ],
+});
